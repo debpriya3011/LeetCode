21dad8596c0680183002a6518ef45d4c9b44248d
debpriya3011 <167235444+debpriya3011@users.noreply.github.com>
2026-09-20 11:41:59 +0530

    Time: 11 ms (21.20%) | Memory: 19.4 MB (97.96%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,10 +1,13 @@
 class Solution:
     def permuteUnique(self, nums: list[int]) -> list[list[int]]:
         from itertools import permutations
-        a = permutations(nums)
-        b = []
-        for i in a :
-            if list(i) not in b:
-                b.append(list(i))
+        a = set(permutations(nums))
+        print(a)
+        b = [list(i) for i in a]
         return b
-        
+        # a = permutations(nums)
+        # b = []
+        # for i in a :
+        #     if list(i) not in b:
+        #         b.append(list(i))
+    
